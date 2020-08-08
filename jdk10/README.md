334745c5ba94ea067204d265e597e0f19e25229a
ankang <ankang@soyoung.com>
2020-08-09 03:13:45 +0800

    添加java10新特性

--- jdk10/README.md
+++ jdk10/README.md
@@ -1,3 +1,5 @@
 # jdk10 新特性学习
 
 ankang.java10.learn.variable 记录“局部变量和类型推断”新特性学习笔记
+
+ankang.java10.learn.collection 记录“集合”性特性学习笔记
--- jdk10/src/main/java/ankang/java10/learn/collection/Collection.java
+++ jdk10/src/main/java/ankang/java10/learn/collection/Collection.java
@@ -1,4 +1,4 @@
-package ankang.java10.learn.list;
+package ankang.java10.learn.collection;
 
 import java.util.ArrayList;
 import java.util.List;
